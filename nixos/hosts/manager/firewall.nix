eb59d8175dede27382e4df449e6f031299800f73
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-07-03 23:48:11 -0500

    feat: remove syncthing and myspeed stacks

--- nixos/hosts/manager/firewall.nix
+++ nixos/hosts/manager/firewall.nix
@@ -11,14 +11,11 @@
       443 # HTTPS
       2377 # Docker Swarm
       7946 # Docker Swarm
-      22000 # Syncthing transfer
     ];
     allowedUDPPorts = [
       53 # DNS
       4789 # Docker Swarm
       7946 # Docker Swarm
-      21027 # Syncthing discovery
-      22000 # Syncthing transfer
       51820 # Wireguard
     ];
   };
