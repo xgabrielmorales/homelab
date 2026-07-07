1794d47b269041139980c426fbc375dc0b96e64c
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-07-07 09:15:43 -0500

    feat(nixos): grant wheel group trusted-users access in nix config

--- nixos/modules/core/nix.nix
+++ nixos/modules/core/nix.nix
@@ -3,6 +3,7 @@
 {
   nixpkgs.config.allowUnfree = true;
   nix.settings.auto-optimise-store = true;
+  nix.settings.trusted-users = [ "root" "@wheel" ];
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
