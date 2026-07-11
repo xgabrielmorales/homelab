cffcec98588d30c37517dc64e6dd3bb19288e9b2
Gabriel Morales <gabrielmorales1@protonmail.com>
2026-07-11 00:09:44 -0500

    chore(nixos): drop swarm firewall ports after k3s migration

--- nixos/hosts/manager/firewall.nix
+++ nixos/hosts/manager/firewall.nix
@@ -9,18 +9,13 @@
       53 # DNS
       80 # HTTP
       443 # HTTPS
-      2377 # Docker Swarm
-      7946 # Docker Swarm
       6443 # k3s API (kubectl)
-      8000 # k3s Traefik catch-all entrypoint
       10250 # k3s kubelet
       6881 # qBittorrent peers (hostPort)
       2222 # forgejo git SSH (hostPort)
     ];
     allowedUDPPorts = [
       53 # DNS
-      4789 # Docker Swarm
-      7946 # Docker Swarm
       51820 # Wireguard
       8472 # k3s flannel VXLAN
       6881 # qBittorrent peers (hostPort)
--- nixos/hosts/worker/firewall.nix
+++ nixos/hosts/worker/firewall.nix
@@ -6,13 +6,9 @@
     allowPing = true;
     allowedTCPPorts = [
       22 # SSH
-      2377 # Docker Swarm
-      7946 # Docker Swarm
       10250 # k3s kubelet
     ];
     allowedUDPPorts = [
-      7946 # Docker Swarm
-      4789 # Docker Swarm
       8472 # k3s flannel VXLAN
     ];
   };
